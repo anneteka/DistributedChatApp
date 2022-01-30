700ffc48990a899815faf5df64dad5021536642c
Anna <a.karlysheva@ukma.edu.ua>
2022-01-30 16:57:28 +0100

    added comments

--- src/main/java/MainClient.java
+++ src/main/java/MainClient.java
@@ -8,6 +8,7 @@ import java.net.DatagramSocket;
 public class MainClient {
 
     public static void main(String[] args) throws Exception {
+        //hostname of the server should be entered here. hostname is printed into the console after the server started
         String host = "DESKTOP-8DHRR0H";
         DatagramSocket socket = new DatagramSocket();
         MessageReceiver r = new MessageReceiver(socket);
@@ -21,6 +22,8 @@ public class MainClient {
 
 class SecondClient{
     public static void main(String[] args) throws Exception {
+        // you can also start a second client
+        // i am not sure if all of this is working on multiple computers but it is a start
         String host  = "DESKTOP-8DHRR0H";
         DatagramSocket socket = new DatagramSocket();
         MessageReceiver r = new MessageReceiver(socket);
