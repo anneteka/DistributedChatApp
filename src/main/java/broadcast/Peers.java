dfcdd5379c5bcb508601868c608ba35923696df2
adarshps22 <23036785+adarshps22@users.noreply.github.com>
2022-02-13 18:27:10 +0100

    Dynamic Discovery added Peers

---
+++ src/main/java/broadcast/Peers.java
@@ -0,0 +1,42 @@
+package broadcast;
+
+import com.sun.nio.sctp.PeerAddressChangeNotification;
+import election.data.PeerInfo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Peers implements Serializable {
+    public enum Flag {
+        REPLY, ACK, BROADCAST, EMPTY
+    }
+
+    private List<PeerInfo> peers;
+    private Flag flag = Flag.EMPTY;
+
+    public Peers() {
+        peers = new ArrayList<PeerInfo>();
+    }
+
+    public List<PeerInfo> getPeers() {
+        return peers;
+    }
+
+    public void addPeer(PeerInfo peer) {
+        this.peers.add(peer);
+    }
+
+    public void clear(){
+        this.peers.clear();;
+    }
+
+    public Flag getFlag() {
+        return flag;
+    }
+
+    public void setFlag(Flag flag) {
+        this.flag = flag;
+    }
+
+}
